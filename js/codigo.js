96194a10f48df6d91b9f87267b688d1d63bd0a32
Jbujeda5 <jbujeda@iesfuentesanluis.org>
2018-01-19 13:12:17 +0100

    Creacion de los tabs y del lector de camara

---
+++ js/codigo.js
@@ -0,0 +1,13 @@
+var video = document.querySelector("#videoElement");
+
+
+
+navigator.mediaDevices.getUserMedia({
+        video: true
+    })
+    .then(function (stream) {
+        video.src = window.URL.createObjectURL(stream);
+    })
+    .catch(function (error) {
+        console.log('error:' + error);
+    })
