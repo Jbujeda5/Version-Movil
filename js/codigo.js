bd764305a1444290a35dab67c5bba41002946889
Jbujeda5 <jbujeda@iesfuentesanluis.org>
2018-01-23 13:16:24 +0100

    Cambios en la venta de pedidos

--- js/codigo.js
+++ js/codigo.js
@@ -1,13 +1,11 @@
-var video = document.querySelector("#videoElement");
-
-
-
-navigator.mediaDevices.getUserMedia({
-        video: true
-    })
-    .then(function (stream) {
-        video.src = window.URL.createObjectURL(stream);
-    })
-    .catch(function (error) {
-        console.log('error:' + error);
-    })
+//var video = document.querySelector("#videoElement");
+//
+//navigator.mediaDevices.getUserMedia({
+//        video: true
+//    })
+//    .then(function (stream) {
+//        video.src = window.URL.createObjectURL(stream);
+//    })
+//    .catch(function (error) {
+//        console.log('error:' + error);
+//    })
