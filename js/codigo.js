fe2ad03f8c7665253d37128281e7c3a28e3e1c12
unknown <rmunoz@iesfuentesanluis.org>
2018-01-24 13:40:53 +0100

    Nada

--- js/codigo.js
+++ js/codigo.js
@@ -1,37 +1,32 @@
 
 $(function(){
-  $('button#confirmarCompra').on('click', confirmarCompra);
+
+  $("#confirmarCompra").click(abrir);
+
   $('button#borrarCompra').on('click', borrarCompra);
 
 
   function borrarCompra(){
-    alert("Compra Borrada");
+      $('#dialog').dialog();
   }
-  function confirmarCompra(){
-    alert("Compra Confirmada");
-  }
-});
-var video = document.querySelector("#videoElement");
 
+  $("#dialog").hide();
 
+    function abrir() {
 
-navigator.mediaDevices.getUserMedia({
-        video: true
-    })
-    .then(function (stream) {
-        video.src = window.URL.createObjectURL(stream);
-    })
-    .catch(function (error) {
-        console.log('error:' + error);
+		$("#dialog").show();
+
+    $( "#dialog" ).dialog({
+      dialogClass: "no-close",
+      buttons: [
+        {
+          text: "OK",
+          click: function() {
+            $( this ).dialog( "close" );
+          }
+        }
+      ]
     });
-//var video = document.querySelector("#videoElement");
-//
-//navigator.mediaDevices.getUserMedia({
-//        video: true
-//    })
-//    .then(function (stream) {
-//        video.src = window.URL.createObjectURL(stream);
-//    })
-//    .catch(function (error) {
-//        console.log('error:' + error);
-//    })
+	}
+
+});
