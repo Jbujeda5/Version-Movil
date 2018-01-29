19096d316c5ec6b25a26d4420eef636052a42e07
Rubén <rmunoz@iesfuentesanluis.org>
2018-01-29 11:19:43 +0100

    Mejora de Alerts en general

--- js/codigo.js
+++ js/codigo.js
@@ -4,13 +4,16 @@ $(function() {
 
   // $('button#borrarCompra').on('click', borrarCompra);
 
-  $('#menuFavCorrecto').on('click', mostrarCorrectoPedidoFav);
+  // $('#menuFavCorrecto').on('click', mostrarCorrectoPedidoFav);
+  $('#menuFavCorrecto').on('click', mostrarAlertConfirmarCompraFav);
   $('#menuFavIncorrecto').on('click', mostrarIncorrectoPedidoFav);
   $('#cerrarFavCorrecto').on('click', cerrarCorrectoPedidoFav);
   $('#cerrarFavIncorrecto').on('click', cerrarIncorrectoPedidoFav);
   $('#confirmarCompra').on('click', mostrarAlertConfirmarCompra);
   $('#botonConfirmarCompra').on('click', cerrarAlertConfirmarCompra);
   $('#botonDenegarCompra').on('click', cerrarAlertConfirmarCompra);
+  $('#botonConfirmarCompraFav').on('click', confirmarAlertConfirmarCompraFav);
+  $('#botonDenegarCompraFav').on('click', cerrarAlertConfirmarCompraFav);
 
 
 
@@ -20,7 +23,17 @@ $(function() {
 function cerrarAlertConfirmarCompra(){
   $('div#confirmarCompraAlert').slideUp();
   $('button#confirmarCompra').removeAttr("disabled");
-
+}
+function confirmarAlertConfirmarCompraFav(){
+  $('div#confirmarCompraFavAlert').slideUp();
+  $("#containerFav").children().removeAttr("disabled");
+  $("div#anyadirFav").children().removeAttr("disabled");
+  mostrarCorrectoPedidoFav();
+}
+function cerrarAlertConfirmarCompraFav(){
+  $('div#confirmarCompraFavAlert').slideUp();
+  $("#containerFav").children().removeAttr("disabled");
+  $("div#anyadirFav").children().removeAttr("disabled");
 }
 
   function cerrarIncorrectoPedidoFav() {
@@ -50,6 +63,11 @@ function cerrarAlertConfirmarCompra(){
   function mostrarAlertConfirmarCompra(){
     $('div#confirmarCompraAlert').slideDown();
     $('button#confirmarCompra').attr("disabled", "true");
+  }
+  function mostrarAlertConfirmarCompraFav(){
+    $('div#confirmarCompraFavAlert').slideDown();
+    $("#containerFav").children().attr("disabled", "true");
+    $("div#anyadirFav").children().attr("disabled", "true");
   }
 
 });
