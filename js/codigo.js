61c381acef9288c1284a8b8ff854ee75e2dafd3b
Rubén <rmunoz@iesfuentesanluis.org>
2018-01-29 11:36:39 +0100

    Funcionalidad de boton añadir menú favorito

--- js/codigo.js
+++ js/codigo.js
@@ -1,40 +1,57 @@
 $(function() {
-
-  // $("#confirmarCompra").click(abrir);
-
-  // $('button#borrarCompra').on('click', borrarCompra);
-
-  // $('#menuFavCorrecto').on('click', mostrarCorrectoPedidoFav);
-  $('#menuFavCorrecto').on('click', mostrarAlertConfirmarCompraFav);
-  $('#menuFavIncorrecto').on('click', mostrarIncorrectoPedidoFav);
-  $('#cerrarFavCorrecto').on('click', cerrarCorrectoPedidoFav);
-  $('#cerrarFavIncorrecto').on('click', cerrarIncorrectoPedidoFav);
+  // Zona Reservas
   $('#confirmarCompra').on('click', mostrarAlertConfirmarCompra);
   $('#botonConfirmarCompra').on('click', cerrarAlertConfirmarCompra);
   $('#botonDenegarCompra').on('click', cerrarAlertConfirmarCompra);
+
+  // Zona Favoritos Menús
   $('#botonConfirmarCompraFav').on('click', confirmarAlertConfirmarCompraFav);
   $('#botonDenegarCompraFav').on('click', cerrarAlertConfirmarCompraFav);
+  $('#menuFavIncorrecto').on('click', mostrarIncorrectoPedidoFav);
+  $('#cerrarFavCorrecto').on('click', cerrarCorrectoPedidoFav);
+  $('#cerrarFavIncorrecto').on('click', cerrarIncorrectoPedidoFav);
+  $('#menuFavCorrecto').on('click', mostrarAlertConfirmarCompraFav);
+
+  // Zona Favoritos Añadir Menús
+  $('#anyadirFavButton').on('click', mostrarAlertAnyadirFav);
+  $('#confirmarAnyadirMenú').on('click', cerrarAlertAnyadirFav);
+  $('#denegarAnyadirMenú').on('click', cerrarAlertAnyadirFav);
+
 
 
 
   $(".alert").hide();
 
+  function cerrarAlertAnyadirFav() {
+    $('div#alertAnyadirNuevoMenuFav').slideUp();
+    $("#containerFav").children().removeAttr("disabled");
+    $("div#anyadirFav").children().removeAttr("disabled");
+  }
 
-function cerrarAlertConfirmarCompra(){
-  $('div#confirmarCompraAlert').slideUp();
-  $('button#confirmarCompra').removeAttr("disabled");
-}
-function confirmarAlertConfirmarCompraFav(){
-  $('div#confirmarCompraFavAlert').slideUp();
-  $("#containerFav").children().removeAttr("disabled");
-  $("div#anyadirFav").children().removeAttr("disabled");
-  mostrarCorrectoPedidoFav();
-}
-function cerrarAlertConfirmarCompraFav(){
-  $('div#confirmarCompraFavAlert').slideUp();
-  $("#containerFav").children().removeAttr("disabled");
-  $("div#anyadirFav").children().removeAttr("disabled");
-}
+  function mostrarAlertAnyadirFav() {
+    $('div#alertAnyadirNuevoMenuFav').slideDown();
+    $("#containerFav").children().attr("disabled", "true");
+    $("div#anyadirFav").children().attr("disabled", "true");
+  }
+
+
+  function cerrarAlertConfirmarCompra() {
+    $('div#confirmarCompraAlert').slideUp();
+    $('button#confirmarCompra').removeAttr("disabled");
+  }
+
+  function confirmarAlertConfirmarCompraFav() {
+    $('div#confirmarCompraFavAlert').slideUp();
+    $("#containerFav").children().removeAttr("disabled");
+    $("div#anyadirFav").children().removeAttr("disabled");
+    mostrarCorrectoPedidoFav();
+  }
+
+  function cerrarAlertConfirmarCompraFav() {
+    $('div#confirmarCompraFavAlert').slideUp();
+    $("#containerFav").children().removeAttr("disabled");
+    $("div#anyadirFav").children().removeAttr("disabled");
+  }
 
   function cerrarIncorrectoPedidoFav() {
     $("div#falloPedidoFav").slideUp();
@@ -60,11 +77,12 @@ function cerrarAlertConfirmarCompraFav(){
     $("div#anyadirFav").children().attr("disabled", "true");
   }
 
-  function mostrarAlertConfirmarCompra(){
+  function mostrarAlertConfirmarCompra() {
     $('div#confirmarCompraAlert').slideDown();
     $('button#confirmarCompra').attr("disabled", "true");
   }
-  function mostrarAlertConfirmarCompraFav(){
+
+  function mostrarAlertConfirmarCompraFav() {
     $('div#confirmarCompraFavAlert').slideDown();
     $("#containerFav").children().attr("disabled", "true");
     $("div#anyadirFav").children().attr("disabled", "true");
